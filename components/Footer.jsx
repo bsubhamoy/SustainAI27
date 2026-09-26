dab6d39b8cf7bbd905dd3c307e069387641deb6d
bsubhamoy <subhamoybanerjee@gmail.com>
2026-09-26 17:42:16 +0530

    Delete components directory

--- components/Footer.jsx
+++
@@ -1,2 +0,0 @@
-import {eventConfig as e} from '../lib/config';
-export default function Footer(){return <footer><div className="wrap foot"><div><strong>{e.name}</strong><div>{e.dateDisplay} · {e.city}</div></div><div>{e.organizer}</div></div></footer>}
