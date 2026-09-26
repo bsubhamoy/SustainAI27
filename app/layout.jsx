9be3f1d856266b64a6538498f38cf4c8c7473d06
bsubhamoy <subhamoybanerjee@gmail.com>
2026-09-26 17:41:45 +0530

    Delete app directory

--- app/layout.jsx
+++
@@ -1,9 +0,0 @@
-import './globals.css';
-import Nav from '../components/Nav';
-import Footer from '../components/Footer';
-import VisualTheme from '../components/VisualTheme';
-
-export const metadata = { title: 'Sust@inAI 27 | Sydney 2027', description: 'International Conference on Sustainable Artificial Intelligence and Green Computing' };
-export default function Layout({ children }) {
-  return <html lang="en"><body><VisualTheme/><Nav/><main>{children}</main><Footer/></body></html>;
-}
